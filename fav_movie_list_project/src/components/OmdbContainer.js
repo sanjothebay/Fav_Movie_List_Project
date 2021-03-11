791b997b7229af0f825e69a0574cef0272f5e930
JC <sanjothebay@gmail.com>
2021-03-10 19:52:08 -0600

    bootstrap not working

---
+++ fav_movie_list_project/src/components/OmdbContainer.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import SearchForm from "./SearchForm";
+import MovieDetail from "./MovieDetail";
+import API from "../utils/API";
+
+class OmdbContainer extends Component {
+  state = {
+    result: {},
+    search: ""
+  };
+
+  // When this component mounts, search for the movie "The Matrix"
+  componentDidMount() {
+    this.searchMovies("soul");
+  }
+
+  searchMovies = query => {
+    API.search(query)
+      .then(res => {
+        console.log(res)
+        this.setState({ result: res.data.results[0] })})
+
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchMovies(this.state.search);
+  };
+
+  render() {
+        const {movies} = this.state.result
+    console.log(movies)
+    return (
+      <Container>
+        <Row>
+          <Col size="md-8">
+            <Card
+              heading={this.state.result.title || "Search for a Movie to Begin"}
+            >
+              {this.state.result.title ? (
+                <MovieDetail
+                  title={this.state.result.title}
+                  src={this.state.result.poster_path}
+                  director={this.state.result.director}
+                  genre={this.state.result.genre}
+                  released={this.state.result.released}
+                />
+              ) : (
+                <h3>No Results to Display</h3>
+              )}
+            </Card>
+          </Col>
+          <Col size="md-4">
+            <Card heading="Search">
+              <SearchForm
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default OmdbContainer;
