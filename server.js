2c1071dbfac2fb0c554a7a7b43878b96e6e60bd1
JC <sanjothebay@gmail.com>
2021-03-17 20:28:15 -0600

    added Client and inder files and folders app shows a GET Error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -10,8 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
+
+// If no API routes are hit, send the React app
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Add routes, both API and view
 app.use(routes);
 
