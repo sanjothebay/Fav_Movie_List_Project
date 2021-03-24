c589a222ee2292b70f08ebcf8c9935783af8bf73
Eduardo Coello <jeck24@hotmail.com>
2021-03-23 21:20:27 -0600

    dlkfdsfoj

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 
 function NavTab(props) {
   return (
       <Navbar bg="dark" variant="dark">
 
         <Navbar.Brand href="#index" onClick={() => props.handlePageChange("Index")}
-        class={
+        className={
           props.currentPage === "Index" ? "nav-link active" : "nav-link"
         }>Movie Watch List</Navbar.Brand>
 
@@ -40,10 +37,6 @@ function NavTab(props) {
           }>Stats</Nav.Link>
 
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
       </Navbar>
   );
 }
