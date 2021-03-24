04620b7c8d8b9f9c76e245740c1d9ae2a918daa2
Eduardo Coello <jeck24@hotmail.com>
2021-03-23 20:20:49 -0600

    NavBar fixed

--- client/src/components/FriendWatchList.js
+++ client/src/components/FriendWatchList.js
@@ -1,12 +1,55 @@
 import React from "react";
 //import "../styles/style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
-function Stats() {
+function FriendWatchList() {
   return (
-    <container class="contactcard">
-      <h1>Friend Watch List will be here</h1>
-    </container>
+  <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Movie Name</th>
+        <th>Genre</th>
+        <th>API Rating</th>
+        <th>My Rating</th>
+        <th>Add to my WatchList</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Movie Name 1</td>
+        <td>Genre 1</td>
+        <td>API Rating 1</td>
+        <td>My Rating 1</td>
+        <td>
+          <Button variant="info">Add</Button>
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Movie Name 2</td>
+        <td>Genre 2</td>
+        <td>API Rating 2</td>
+        <td>My Rating 2</td>
+        <td>
+          <Button variant="info">Add</Button>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Movie Name 3</td>
+        <td>Genre 3</td>
+        <td>API Rating 3</td>
+        <td>My Rating 3</td>
+        <td>
+          <Button variant="info">Add</Button>
+        </td>
+      </tr>
+    </tbody>
+  </Table>
   );
 }
-export default Stats;
+export default FriendWatchList;
--- client/src/components/MyWatchList.js
+++ client/src/components/MyWatchList.js
@@ -1,43 +1,84 @@
 import React from "react";
 //import "../styles/style.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import Table from 'react-bootstrap/Table';
+import InputGroup from 'react-bootstrap/InputGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function MyWatchList() {
   return (
-    <container class="contactcard">
-      <h1>My Watch List will be here</h1>
-      <h2>Favourites Movies:</h2>
-      <table class="table">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    </container>
+  <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Movie Name</th>
+        <th>Genre</th>
+        <th>API Rating</th>
+        <th>My Rating</th>
+        <th>Watched</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Movie Name 1</td>
+        <td>Genre 1</td>
+        <td>API Rating 1</td>
+        <td>My Rating 1</td>
+        <td>
+          <InputGroup className="mb-3">
+            <DropdownButton
+            as={InputGroup.Prepend}
+            variant="outline-secondary"
+            title="Already Watched?"
+            id="input-group-dropdown-1">
+              <Dropdown.Item href="#">Yes</Dropdown.Item>
+              <Dropdown.Item href="#">No</Dropdown.Item>
+              </DropdownButton>
+          </InputGroup>
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Movie Name 2</td>
+        <td>Genre 2</td>
+        <td>API Rating 2</td>
+        <td>My Rating 2</td>
+        <td>
+          <InputGroup className="mb-3">
+            <DropdownButton
+            as={InputGroup.Prepend}
+            variant="outline-secondary"
+            title="Already Watched?"
+            id="input-group-dropdown-2">
+              <Dropdown.Item href="#">Yes</Dropdown.Item>
+              <Dropdown.Item href="#">No</Dropdown.Item>
+              </DropdownButton>
+          </InputGroup>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Movie Name 3</td>
+        <td>Genre 3</td>
+        <td>API Rating 3</td>
+        <td>My Rating 3</td>
+        <td>
+          <InputGroup className="mb-3">
+            <DropdownButton
+            as={InputGroup.Prepend}
+            variant="outline-secondary"
+            title="Already Watched?"
+            id="input-group-dropdown-3">
+              <Dropdown.Item href="#">Yes</Dropdown.Item>
+              <Dropdown.Item href="#">No</Dropdown.Item>
+              </DropdownButton>
+          </InputGroup>
+        </td>
+      </tr>
+    </tbody>
+  </Table>
   );
 }
 export default MyWatchList;
